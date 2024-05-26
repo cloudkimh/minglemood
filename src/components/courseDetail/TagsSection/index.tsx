8507ac3a04ca94cad8772f5bfd5f4693691c18b4
haeineeeee <godls036@gmail.com>
2024-05-26 21:22:52 +0900

    Fix: courseDetail에 있는 TagSection 스타일링

--- src/components/courseDetail/TagsSection/index.tsx
+++ src/components/courseDetail/TagsSection/index.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { SectionContainer, SectionHeader, SectionTitle } from "../styles";
 import RoundTag from "./RoundTag";
+import { HorizontalBar } from "../../common/styles/Common";
+import palette from "../../../lib/styles/palette";
 
 export type TagsSectionProps = {
     tags: Array<string>;
@@ -14,57 +16,60 @@ function TagsSection(props: TagsSectionProps) {
     const { tags } = props;
 
     return (
-        <SectionContainer>
-            <SectionRow>
-                <StyledSectionHeader>
-                    <SectionTitle>포함 사항</SectionTitle>
-                </StyledSectionHeader>
-                <TagGrid>
-                    {tagList1.map((aTag, i) => (
-                        <StyledRoundTag
-                            key={`tag-${i}`}
-                            color="green"
-                            text={aTag}
-                        />
-                    ))}
-                </TagGrid>
-            </SectionRow>
-            <SectionRow>
-                <StyledSectionHeader>
-                    <SectionTitle>비포함 사항</SectionTitle>
-                </StyledSectionHeader>
-                <TagGrid>
-                    {tagList2.map((aTag, i) => (
-                        <StyledRoundTag
-                            key={`tag-${i}`}
-                            color="red"
-                            text={aTag}
-                        />
-                    ))}
-                </TagGrid>
-            </SectionRow>
-            <SectionRow>
-                <StyledSectionHeader>
-                    <SectionTitle>준비물</SectionTitle>
-                </StyledSectionHeader>
-                <TagGrid>
-                    {tagList3.map((aTag, i) => (
-                        <StyledRoundTag key={`tag-${i}`} text={aTag} />
-                    ))}
-                </TagGrid>
-            </SectionRow>
-        </SectionContainer>
+        <>
+            <SectionContainer>
+                <SectionRow>
+                    <StyledSectionHeader>
+                        <SectionTitle>준비물</SectionTitle>
+                    </StyledSectionHeader>
+                    <TagGrid>
+                        {tagList3.map((aTag, i) => (
+                            <StyledRoundTag key={`tag-${i}`} text={aTag} />
+                        ))}
+                    </TagGrid>
+                </SectionRow>
+                <SectionRow>
+                    <StyledSectionHeader>
+                        <SectionTitle>포함 사항</SectionTitle>
+                    </StyledSectionHeader>
+                    <TagGrid>
+                        {tagList1.map((aTag, i) => (
+                            <StyledRoundTag
+                                key={`tag-${i}`}
+                                // color="green"
+                                text={aTag}
+                            />
+                        ))}
+                    </TagGrid>
+                </SectionRow>
+                <SectionRow>
+                    <StyledSectionHeader>
+                        <SectionTitle>불포함 사항</SectionTitle>
+                    </StyledSectionHeader>
+                    <TagGrid>
+                        {tagList2.map((aTag, i) => (
+                            <StyledRoundTag
+                                key={`tag-${i}`}
+                                color="red"
+                                text={aTag}
+                            />
+                        ))}
+                    </TagGrid>
+                </SectionRow>
+            </SectionContainer>
+            <StyledHorizontalBar />
+        </>
     );
 }
 
 const SectionRow = styled.div`
     & + & {
-        margin-top: 45px;
+        margin-top: 25px;
     }
 `;
 
 const StyledSectionHeader = styled(SectionHeader)`
-    margin-bottom: 16px;
+    margin-bottom: 7px;
 `;
 
 const TagGrid = styled.div`
@@ -72,11 +77,16 @@ const TagGrid = styled.div`
     flex-wrap: wrap;
     max-width: 287px;
     padding: 0px 20px;
+    row-gap: 12px;
 `;
 
 const StyledRoundTag = styled(RoundTag)`
     margin-right: 9px;
-    margin-bottom: 12px;
+`;
+
+const StyledHorizontalBar = styled(HorizontalBar)`
+    height: 8px;
+    background-color: ${palette.white3};
 `;
 
 export default TagsSection;
